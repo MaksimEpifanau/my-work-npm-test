61165021202ab686b9ca4e8968e84b30677aaf39
Maverick17user <maksim.epifanov.2000@mail.ru>
2021-02-26 10:00:35 +0300

    update scripts to build appropriate lib version

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HelloWorld() {
-	return <h1 styles={{ color: "blue" }} >Hello World</h1>;
+	return <h1 style={{ color: "blue" }} >Hello Wor</h1>;
 }
 
 export default HelloWorld;
